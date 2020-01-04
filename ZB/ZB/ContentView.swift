fcc5673f83919a966fff7d9e863a12baff7d2eb3
Clxxxxxxx <ClxxxxzzZ@163.com>
2020-01-04 16:45:02 +0800

    初始化

---
+++ ZB/ZB/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ZB
+//
+//  Created by 30 on 2020/1/4.
+//  Copyright © 2020 iflytek. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
